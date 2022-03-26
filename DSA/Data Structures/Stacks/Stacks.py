34254d766e4614d46708236d879e6cc5c32fd16a
Bilal Ahmed <bilalahmed2472@gmail.com>
2022-03-27 02:34:29 +0500

    Add files via upload

--- DSA/Data Structures/Stacks/Stacks.py
+++ DSA/Data Structures/Stacks/Stacks.py
@@ -1,10 +1,17 @@
 
-arr = [2, 3, 5, 10, 11]
-i=1
-print("After iteration")
-arr = [2, 3, 5, 10, 11]
-i=1
-print("After iteration")
-arr = [2, 3, 5, 10, 11]
-i=1
-print("After iteration")
+#Stacks
+import random
+print("Demonstration of Stack using LIFO, Last In First Out ")
+arr = []
+print("Array:",arr)
+j = 1
+while len(arr) < 5:
+    n = random.randint(0, 20)
+    arr.append(n)
+    print("Array Last IN: After", j, "iteration", arr)
+    j = j+1
+i = 1
+while len(arr) > 0:
+    arr.pop()
+    print("Array First OUT: After", i, "iteration", arr)
+    i=i+1
