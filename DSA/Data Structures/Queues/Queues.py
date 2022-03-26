f85145f8f923c9f4cbc44f796b7a898782807641
Bilal Ahmed <bilalahmed2472@gmail.com>
2022-03-27 03:09:43 +0500

    Add files via upload

--- DSA/Data Structures/Queues/Queues.py
+++ DSA/Data Structures/Queues/Queues.py
@@ -1,2 +1,18 @@
 
-print("Hello World")
+# Queues
+import random
+print("Implementing Queue First IN First OUT")
+que = []
+print("Queue:", que)
+i=1
+while len(que) < 4:
+    n = random.randint(0, 20)
+    que.insert(0, n)
+    print("Queue: After First IN:", i, "iteration", que)
+    i = i+1
+j = 1
+while len(que) > 0:
+    que.pop()
+    print("Queue: After First OUT:", j, "iteration", que)
+    j = j + 1
+
